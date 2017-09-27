3abfd368281b94d2d193b1efdac6c8d124e67457
valeriadelrio <valeriadlrio@gmail.com>
2017-09-27 19:40:46 -0300

    Eventos en el mapa. Creacion y arrastre de marcadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MapasService } from './services/mapas.service';
+import { Marcador } from "./interfaces/marcador.interface";
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,20 @@ export class AppComponent {
   lat: number = -37.328618;
   lng: number = -59.135322;
   zoom: number = 17;
+
+  constructor (public _ms:MapasService){
+
+  }
+  clickMapa(evento){
+    let nuevoMarcador:Marcador = {
+      lat: evento.coords.lat,
+      lng: evento.coords.lng,
+      titulo: "Sin titulo",
+      draggable: true
+    }
+
+    this._ms.insertarMarcador(nuevoMarcador);
+    console.log(evento);
+
+  }
 }
--- src/app/services/mapas.service.ts
+++ src/app/services/mapas.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Marcador } from "../interfaces/marcador.interface";
 
 @Injectable()
 export class MapasService {
 
-  constructor() { }
+  marcadores: Marcador[] = [];
+
+  constructor() {
+    let nuevoMarcador: Marcador = {
+      lat:-37.328618,
+      lng:-59.135322,
+      titulo: "Plaza central",
+      draggable: true
+    }
+    this.marcadores.push(nuevoMarcador);
+  }
+
+  insertarMarcador(marcador:Marcador){
+    this.marcadores.push(marcador);
+
+  }
+
+
 
 }
